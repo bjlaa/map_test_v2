b91b2d445cf853955cea7878fd49e9715a8d353a
Maxodoui <maxime.gadras@gmail.com>
2018-12-03 22:56:31 +0100

    div added to map/searchbar/searchresults (pas fonctionnel encore)

--- src/components/SearchResults/SearchResults.vue
+++ src/components/SearchResults/SearchResults.vue
@@ -1,35 +1,36 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div v-if="isSearchResultsOpen && searchInputValue !== '' && searchResults" class="searchResults">
+            <ul class="searchList">
+              <!-- On render seulement si il y a des résultats de recherche -->
+              <li
+                v-if='searchResults.length > 0'
+                v-for="item in searchResults"
+                class="searchListItem"
+                @click="addPin(item)"
+              >
+                <div class="searchListItemIcon">
+                  <img src="./assets/markerIcon.png" alt="">
+                </div>
+                <div class="searchListItemText">
+                  <div class="searchListItemTextName">
+                    {{ item.name }}
+                  </div>
+                  <div class="searchListItemTextAddress">
+                    {{ item.location && item.location.address1 }}, {{ item.location && item.location.zip_code }} {{ item.location && item.location.city }}
+                  </div>
+                </div>
+              </li>
+              <!-- Si pas de résultats on render: -->
+              <li
+                v-if='searchResults.length <= 0'
+                class="searchListItem"
+              >
+                <div class="searchListItemText">
+                  No result found
+                </div>
+              </li>
+            </ul>
+          </div>
 </template>
 
 <script>
