4fd6ade6920ad9f916b0a4afcbf257a87e4ad28b
Maxodoui <maxime.gadras@gmail.com>
2018-12-05 16:33:40 +0100

    Added PinList / PinListItem + reviews

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,29 +1,26 @@
 <template>
+  <aside class="sideBar card">
+        <div class="sideBarLogo">
+          <img class="sideBarLogoImage" src="./assets/Wigot_Logo.png">
+        </div>
+        <div v-if="currentEvent.title" class="sideBarHeader">
+          <div class="sideBarHeaderTitle">
+            {{ currentEvent.title }}
+          </div>
+          <div v-if="currentEvent.author" class="sideBarHeaderAuthor">
+            Created by {{ currentEvent.author }}
+          </div>
+        </div>
+        <PinList />
+  </aside>
 
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-};
+  import PinList from './components/PinList/PinList.vue'
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-h3
-  margin 40px 0 0
 
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
 </style>
